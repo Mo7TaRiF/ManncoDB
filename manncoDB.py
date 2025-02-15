5662fd3e9b89e80ec1da6e070ab41f70e1ccdba7
Mo7TaRiF <eng.mo7tarif@gmail.com>
2025-02-15 18:46:37 +0200

    Create the project

--- manncoDB.py
+++ manncoDB.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from time import sleep
 
 url = "https://mannco.store/item/440-mann-co-supply-crate-key"
 
 driver = webdriver.Chrome()
 driver.get(url)
 print(driver.find_element(By.XPATH, value='//*[@id="transacContent"]/tr[1]/td[3]').text)
+
+sleep(3600)
