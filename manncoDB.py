3efa6ce57c7d261b3aa997b5d3e12d270c107b8f
Mo7TaRiF <eng.mo7tarif@gmail.com>
2025-02-15 18:51:16 +0200

    Create the project

--- manncoDB.py
+++ manncoDB.py
@@ -1,10 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Run in headless mode
+chrome_options.add_argument("--no-sandbox")  # Bypass OS security model
+chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
+
+driver = webdriver.Chrome(options=chrome_options)
 
 url = "https://mannco.store/item/440-mann-co-supply-crate-key"
 
-driver = webdriver.Chrome()
 driver.get(url)
 print(driver.find_element(By.XPATH, value='//*[@id="transacContent"]/tr[1]/td[3]').text)
 
